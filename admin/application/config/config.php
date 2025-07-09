2a48d69ce785a37826d29a6a9f33780d4829487d
NTMacBook <ntmacbook@MacBook-Pro.local>
2025-07-09 16:30:55 +0530

    api secured

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://bastian.ninetriangles.com/admin';
+$config['base_url'] = 'http://localhost/bastian-admin';
 $config['enable_cors'] = TRUE;
 /*
 |--------------------------------------------------------------------------
